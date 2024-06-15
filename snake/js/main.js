252a1ce2ddfb3a08d5ca61210a94faef33fcea70
Ayman El <itcanbeanythingreally@gmail.com>
2024-06-15 15:39:24 +0100

    WOOO YEAH BABY

--- snake/js/main.js
+++ snake/js/main.js
@@ -1,5 +1,18 @@
-import { DOM, GameModes } from './constants.js';
+import DOM from './gameDOM.js';
 import Game from './game.js';
 
 // Event Listeners
+DOM.restart.addEventListener("click", Game.startGame);
+document.addEventListener("keydown", (evt) => {
+    if(Game.moved) return;
+    
+    Game.moved = true;
+    Game.changeDir(evt.key);
+});
 
+DOM.mobileButtons[0].addEventListener("click", () => Game.changeDir("ArrowUp"));
+DOM.mobileButtons[1].addEventListener("click", () => Game.changeDir("ArrowLeft"));
+DOM.mobileButtons[2].addEventListener("click", () => Game.changeDir("ArrowDown"));
+DOM.mobileButtons[3].addEventListener("click", () => Game.changeDir("ArrowRight"));
+
+Game.startGame();
