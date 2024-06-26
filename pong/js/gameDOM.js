dae508c93cb52b138f705f786f680d76d7bc844b
Ayman El <itcanbeanythingreally@gmail.com>
2024-06-26 22:53:41 +0100

    This pong is not unlocked yet

---
+++ pong/js/gameDOM.js
@@ -0,0 +1,10 @@
+const DOM = {
+    gameScore: $('game-score'),
+    gameRender: $('game-render'),
+    player: $q('.player'),
+    ball: $q('.ball'),
+    opponent: $q('.opponent')
+};
+
+
+export default DOM;
