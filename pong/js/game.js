ee7059901148c6eccd9ed40fa30c49cb9871293d
Ayman El <itcanbeanythingreally@gmail.com>
2024-06-27 11:03:35 +0100

    Pong Cheap Clone

--- pong/js/game.js
+++ pong/js/game.js
@@ -3,11 +3,14 @@ import DOM from './gameDOM.js';
 let gameState = {
     playerTop: .5,
     opponentTop: .5,
-    ball: [ .48, .495 ],
+    ball: [.48, .48],
+    ballVel: [0, 1],
+    scores: [0, 0],
 };
 
 const Game = {
-    paddleHeight: .08
+    paddleHeight: .08,
+    ballSize: .2,
 };
 
 export default Game;
--- pong/js/gameDOM.js
+++ pong/js/gameDOM.js
@@ -1,9 +1,16 @@
 const DOM = {
-    gameScore: $('game-score'),
-    gameRender: $('game-render'),
+    score: $('game-score'),
+    render: $('game-render'),
     player: $q('.player'),
     ball: $q('.ball'),
-    opponent: $q('.opponent')
+    opponent: $q('.opponent'),
+    updateScore (player, opponent){
+        DOM.score.innerText = `Player ${player} : ${opponent} Opponent`;
+    },
+    updateBall (ball){
+        DOM.ball.top = `${ball[0] / 3}%`;
+        DOM.ball.right = `${ball[1] / 3}%`;
+    }
 };
 
 
